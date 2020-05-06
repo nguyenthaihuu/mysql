1181ff70420cba82c8688e036219928066a44122
nguyenthaihuu <42740589+nguyenthaihuu@users.noreply.github.com>
2020-05-06 13:56:45 +0700

    Create remove_to_data.sh

---
+++ remove_to_data.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Auto update
+# yum update
+
+# Stop MySQL
+systemctl stop mysqld
+
+# Copy MySQL
+cp -r /var/lib/mysql /data2/mysql
+
+# Rename MySQL
+mv -rf /var/lib/mysql /var/lib/mysql_bk
+
+# Create link MySQL
+ln -s /data2/mysql /var/lib/mysql
+
+# Start MySQL
+systemctl start mysqld
+
+# Status MySQL
+systemctl status mysqld
+
